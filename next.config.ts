c225367796927a4dd814098b78da3b103afea2e5
Jordan <olivier.jordan21@gmail.com>
2025-10-27 02:05:01 +0100

    Disable image optimization for production

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
-    unoptimized: false,
+    unoptimized: true, // Désactive l'optimisation d'images pour éviter les erreurs en prod
   },
 };
 
